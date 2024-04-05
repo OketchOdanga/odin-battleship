c27476208606d9d65ba868ed5b23c71e2dbe8ed6
Austine Odanga <austinoketches@gmail.com>
2024-04-05 12:02:30 +0300

    test: all class Ship functions

--- battleship.test.js
+++ battleship.test.js
@@ -1,6 +1,6 @@
 const {Ship} = require('./battleship');
 describe('Class Ship', () => {
-    const mockShip = new Ship('yatch',5,null,false);
+    const mockShip = new Ship('yatch',5,0,false);
     test('ship name',() => {
         expect(mockShip.name).toBe('yatch');
     });
@@ -8,10 +8,16 @@ describe('Class Ship', () => {
         expect(mockShip.length).toEqual(5);
     });
     test('ship hits',() => {
-        expect(mockShip.hits).toBe(mockShip.hit());
+        expect(mockShip.hits).toBe(0);
 
     });
+    test('hit function', ()=>{
+        expect(mockShip.hit()).toBe(mockShip.hits);
+    })
     test('if the ship has sunk',()=>{
         expect(mockShip.sunk).toBe(false)
+    });
+    test('if the ship has sunk using isSunk',()=>{
+        expect(mockShip.isSunk()).toBe(false)
     });
 })
